02fdcef9a55df3a27f549c09e568aed35f11f49f
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-11-03 18:37:08 +0200

    finished

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// task 2
 const paralelogram = {
     a: 10,
     b: 20
@@ -6,7 +7,8 @@ const paralelogram = {
 const square = () => {
     document.getElementById("square").innerHTML = 'Square of paralelogram:' + 0.5 * paralelogram.a * paralelogram.b;
 };
-
+square();
+//  ============================= task 5 and 1 =============================================
 const onMouse = () => {
     let from3 = document.getElementById("#3").innerText;
     let from6 = document.getElementById("#6").innerText;
@@ -15,6 +17,7 @@ const onMouse = () => {
     document.getElementById('#6').innerHTML = from3;
 };
 
+// ===================================== task 3 ======================================================
 const maxi = () => {
     const form = document.getElementById('form');
     let arr = form.input.value.split('');
@@ -55,6 +58,51 @@ const checkCookie = () => {
     }
 };
 
+// ==================================== task 4 ==============================================
+const checkStorage = () => {
+    let check = document.getElementById('ch1');
+    if (check.checked) {
+        localStorage.setItem('checked', true);
+    }
+    else {
+        localStorage.setItem('checked', false);
+    }
+    goAlignRight();
+};
 
 
-square();
+const goAlignRight = () => {
+    if (localStorage.getItem('checked') === 'true') {
+        document.getElementById('left').style.textAlign = 'right';
+        document.getElementById('right').style.textAlign = 'right';
+    }
+    else {
+        document.getElementById('left').style.textAlign = 'center';
+        document.getElementById('right').style.textAlign = 'center';
+    }
+};
+
+// ================================= task 6 ==============================
+const addText = () => {
+    document.getElementById('spany').innerHTML = document.getElementById('add').value;
+};
+
+const saveText = () =>{
+    let text = document.getElementById('spany').innerText;
+    localStorage.setItem('texty', text)
+    localStorage.setItem('textFlag', true);
+    document.getElementById('textChanging').innerText = localStorage.getItem('texty');
+};
+
+const storage = () => {
+    if (localStorage.getItem('textFlag') === 'true') {
+        document.getElementById('textChanging').innerText = localStorage.getItem('texty');
+    }
+}
+
+const deleteFromLocalStorage = () =>{
+    localStorage.setItem('textFlag', 0);
+    location.reload();
+}
+
+
